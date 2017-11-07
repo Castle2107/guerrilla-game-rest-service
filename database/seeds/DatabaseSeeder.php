40cf992147349c2832dc0084f393464ee9e76d9c
Daniel Castillo <danielcastilloleiva.21@gmail.com>
2017-11-06 21:02:59 -0600

    Default guerrilla_types, battle_units & battle_reso

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,55 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        DB::table('guerrilla_types')->insert([
+        	['name' => 'China',
+        	'description' => 'ching chong hoo'],
+
+        	['name' => 'USMC',
+        	'description' => 'blrs rfdsh sdf fjod'],
+
+        	['name' => 'MEC',
+        	'description' => 'wtf is this sldf jdsfjd']
+        ]);
+
+        DB::table('battle_resources')->insert([
+        	['name' => 'money',
+        	'description' => 'sdijf osd jfosdijfoidis'],
+
+        	['name' => 'people',
+        	'description' => 'sdijf osd jfosdijfoidis'],
+
+        	['name' => 'oil',
+        	'description' => 'sdijf osd jfosdijfoidis'],
+        ]);
+
+        DB::table('battle_units')->insert([
+        	['name' => 'assault',
+        	'description' => 'unidades especializadas en 
+        			infiltrarse en bases enemigas y acabar con la infantería',
+			'type' => 'offense',
+			'cost' => 100],
+
+			['name' => 'engineer',
+        	'description' => 'capaces no solo de construir estructuras de defensa sólidas, sino de
+					destruir unidades mecánicas enemigas, pues siempre están 
+					cargados de todo tipo de explosivos y herramientas.',
+			'type' => 'defense',
+			'cost' => 100],
+
+			['name' => 'tank',
+        	'description' => 'más costosos que la infantería, pero de mayor resistencia y poder
+					de batalla. Es una unidad que no debe faltar en cualquier asalto.',
+			'type' => 'offense',
+			'cost' => 100],
+
+			['name' => 'bunker',
+        	'description' => 'indispensables para dar una buena bienvenida a la guerrilla enemiga que
+					se ha invitado a dar un paseo por nuestras instalaciones.',
+			'type' => 'offense',
+			'cost' => 100],        	
+        ]);
+
+                
     }
 }
