fd3c6bbaaf841922c3f9798870431202cede618e
Daniel Castillo <danielcastilloleiva.21@gmail.com>
2017-11-10 18:13:19 -0600

    Added some API routes for guerrilla model

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,26 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+// Every api route looks like: ip/api/resource
+// If you want to remove the prefix you can edit 
+// the RouteServiceProvider class on /app/Providers/RouteServiceProvider.php
+
+// Deletes edit and create functions
+// Route::apiResource('/items', 'ItemsController');
+
+use App\Models\Guerrilla;
+use App\Http\Resources\Guerrilla as GuerrillaResource;
+
+Route::get('/list_guerrillas', 'GuerrillaController@index');
+
+Route::get('/inspect_guerrilla/{guerrilla}', 'GuerrillaController@show');
+
+Route::post('/create_guerrilla', 'GuerrillaController@store');
+
+Route::post('/guerrillas/battle_units/', 'GuerrillaController@buyBattleUnit');
+
+Route::apiResource('/guerrillas', 'GuerrillaController');
