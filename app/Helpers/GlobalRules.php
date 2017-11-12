09e8dd7361f4007c7dab5bbea1a3a7d69735f1a4
Daniel Castillo <danielcastilloleiva.21@gmail.com>
2017-11-12 17:43:52 -0600

    Updated global rules

--- app/Helpers/GlobalRules.php
+++ app/Helpers/GlobalRules.php
@@ -8,28 +8,32 @@ class GlobalRules {
 		'money' => 20,
 		'people' => 1,
 		'oil' => 25,
-		'points' => 3
+		'points' => 3,
+		'name' => 'assault'
 	);
 
 	const ENGINEER_UNIT = array(
 		'money' => 10,
 		'people' => 1,
 		'oil' => 25,
-		'points' => 3
+		'points' => 3,
+		'name' => 'engineer'
 	);
 
 	const TANK_UNIT = array(
 		'money' => 200,
 		'people' => 8,
 		'oil' => 500,
-		'points' => 17
+		'points' => 17,
+		'name' => 'tank'
 	);
 
 	const BUNKER_UNIT = array(
 		'money' => 300,
 		'people' => 8,
 		'oil' => 200,
-		'points' => 20
+		'points' => 20,
+		'name' => 'bunker'
 	);
 
 
