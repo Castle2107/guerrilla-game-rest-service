b553746cf6194f21c8f383e98847d38ebfe048c7
Bryan <bryan.moralessanchez@ucr.ac.cr>
2017-12-01 18:15:36 -0600

    Added the view of create_guerrilla

---
+++ resources/views/create-guerrilla.blade.php
@@ -0,0 +1,63 @@
+<h4><b>Create Guerrilla</b> - Create a guerrilla</h4>
+
+<p class="label label-primary">POST</p>
+
+<kbd>/create_guerrilla</kbd>
+
+<!-- Request -->
+<h5><b>Request Body</b></h5>
+<div class="relevant-content">
+    <p>
+        <span class="attribute">Content-Type:</span>
+        <span class="value">application/json</span>
+    </p>
+    <p>
+        <span class="attribute">Accept:</span>
+        <span class="value">application/json</span>
+    </p>
+    <p><span class="keyword">{</span></p>
+    <p class="indent">
+        <span class="attribute">"username":</span>
+        <span class="value">"ameseguer",</span>
+    </p>
+    <p class="indent">
+        <span class="attribute">"email":</span>
+        <span class="value">"andres.meseguer@ucr.ac.cr",</span>
+    </p>
+    <p class="indent">
+        <span class="attribute">"faction":</span>
+        <span class="value">"MEC"</span>
+    </p>
+    <p><span class="keyword">}</span></p>
+</div>
+
+<!-- Response -->
+<h5><b>Success Response</b></h5>
+<div class="relevant-content">
+	<p>
+        <span class="attribute">Access-Control-Allow-Origin:</span>
+        <span class="value">*</span>
+    </p>
+    <p>
+        <span class="attribute">Content-Type:</span>
+        <span class="value">application/json</span>
+    </p>
+    <p>
+        <span class="attribute">Date:</span>
+        <span class="value">Fri, 01 Dec 2017 20:51:33 +0000, Fri, 01 Dec 2017 20:51:33 GMT</span>
+    </p>
+    <p>
+        <span class="attribute">Host:</span>
+        <span class="value">localhost:8000</span>
+    </p>
+    <p><span class="keyword">{</span></p>
+    <p class="indent">
+        <span class="attribute">"username":</span>
+        <span class="value">"ameseguer",</span>
+    </p>
+    <p class="indent">
+        <span class="attribute">"id":</span>
+        <span class="value">100</span>
+    </p>
+    <p><span class="keyword">}</span></p>
+</div>
