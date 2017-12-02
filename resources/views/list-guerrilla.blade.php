49e15f5ea9c0499479a07e1424097295995bfd90
Bryan <bryan.moralessanchez@ucr.ac.cr>
2017-12-01 18:16:37 -0600

    Added the view of list_guerrilla

---
+++ resources/views/list-guerrilla.blade.php
@@ -0,0 +1,100 @@
+<h4><b>List Guerrillas</b> - List guerrillas</h4>
+
+<p class="label label-success">GET</p>
+
+<kbd>/list_guerrillas</kbd>
+
+<!-- Response -->
+<h5><b>Success Response</b></h5>
+<div class="relevant-content">
+	<p>
+	    <span class="attribute">Access-Control-Allow-Origin:</span>
+	    <span class="value">*</span>
+	</p>
+	<p>
+	    <span class="attribute">Content-Type:</span>
+	    <span class="value">application/json</span>
+	</p>
+	<p>
+	    <span class="attribute">Date:</span>
+	    <span class="value">Fri, 01 Dec 2017 20:51:33 +0000, Fri, 01 Dec 2017 20:51:33 GMT</span>
+	</p>
+	<p>
+	    <span class="attribute">Host:</span>
+	    <span class="value">localhost:8000</span>
+	</p>
+	<p><span class="keyword">[</span></p>
+	<p class="indent">
+		<span class="attribute">{</span>
+	</p>
+	<p class="indent-double">
+		<span class="attribute">"username":</span>
+		<span class="value">"ameseguer",</span>
+	</p>
+	<p class="indent-double">
+		<span class="attribute">"id":</span>
+		<span class="value">100,</span>
+	</p>
+	<p class="indent-double">
+		<span class="attribute">"rank":</span>
+		<span class="value">3</span>
+	</p>
+	<p class="indent">
+		<span class="attribute">},</span>
+	</p>
+	<p class="indent">
+		<span class="attribute">{</span>
+	</p>
+	<p class="indent-double">
+		<span class="attribute">"username":</span>
+		<span class="value">"jdoe",</span>
+	</p>
+	<p class="indent-double">
+		<span class="attribute">"id":</span>
+		<span class="value">101,</span>
+	</p>
+	<p class="indent-double">
+		<span class="attribute">"rank":</span>
+		<span class="value">1</span>
+	</p>
+	<p class="indent">
+		<span class="attribute">},</span>
+	</p>
+	<p class="indent">
+		<span class="attribute">{</span>
+	</p>
+	<p class="indent-double">
+		<span class="attribute">"username":</span>
+		<span class="value">"destructor",</span>
+	</p>
+	<p class="indent-double">
+		<span class="attribute">"id":</span>
+		<span class="value">102,</span>
+	</p>
+	<p class="indent-double">
+		<span class="attribute">"rank":</span>
+		<span class="value">2</span>
+	</p>
+	<p class="indent">
+		<span class="attribute">},</span>
+	</p>
+	<p class="indent">
+		<span class="attribute">{</span>
+	</p>
+	<p class="indent-double">
+		<span class="attribute">"username":</span>
+		<span class="value">"slayer",</span>
+	</p>
+	<p class="indent-double">
+		<span class="attribute">"id":</span>
+		<span class="value">103,</span>
+	</p>
+	<p class="indent-double">
+		<span class="attribute">"rank":</span>
+		<span class="value">4</span>
+	</p>
+	<p class="indent">
+		<span class="attribute">}</span>
+	</p>
+	<p><span class="keyword">]</span></p>
+</div>
